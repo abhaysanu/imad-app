0be0cd0047d6d7540da3eb530cbfed811952d21e
abhaysanu <30664127+abhaysanu@users.noreply.github.com>
2017-08-12 15:51:11 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -12,10 +12,10 @@ app.get('/article-one', function (req, res){
   res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
 app.get('/article-two', function (req, res){
-    res.send('Articl two is being requested and will be served');
+    res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
 });
 app.get('/article-three', function (req, res){
-    res.send('Articl three is being requested and will be served');
+     res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
 });
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
