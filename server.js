bce87315ad2c5725eedee33ba0c88d883f9b5019
abhaysanu <30664127+abhaysanu@users.noreply.github.com>
2017-08-15 14:17:33 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -91,7 +91,7 @@ return htmlTemplate;
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-app.get('/:article-one', function (req, res){
+app.get('/:articleName', function (req, res){
 //artileName == article-one
 //article[articleName] == {} content object for article one
 
