bb9e8adf4ce6a78ab5299f18764841efbfb12202
abhaysanu <30664127+abhaysanu@users.noreply.github.com>
2017-08-20 17:10:10 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -104,7 +104,7 @@ app.get('/ui/style.css', function (req, res) {
 
 var counter=0;
 app.get('/counter', function(req, res){
-    counter+=1;
+    counter = counter+1;
     res.send(counter.toString()) 
 });
 
