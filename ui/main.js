20d66cdba174022929fd70f7d61fa756d32e14ad
abhaysanu <30664127+abhaysanu@users.noreply.github.com>
2017-08-22 15:11:31 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -17,9 +17,9 @@ img.onclick = function () {
   var interval = setInterval(moveRight, 60);  
 };
 
-var button = document.getElementById('counter');
-button.onclick = function () {
-   counter = counter + 1;
-   var span = document.getElementById('count');
-   span.innerHtml = counter;
+    var button = document.getElementById('counter');
+    button.onclick = function () {
+    counter = counter + 1;
+    var span = document.getElementById('count');
+    span.innerHTML = counter.toString();
 };
