8edfeadfb005a0c7958a8d59be527355332d30bb
abhaysanu <30664127+abhaysanu@users.noreply.github.com>
2017-08-31 19:05:26 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -2,6 +2,16 @@ var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 
+var pool = require('pg').pool;
+
+var config = {
+    user: "abhayvs610",
+    database:"abhayvs610",
+    host: "db.imad.hasura-app.io",
+    port: '5432',
+    password: process.env.DB_PASSWORD
+}
+
 var app = express();
 app.use(morgan('combined'));
 
@@ -90,6 +100,19 @@ return htmlTemplate;
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+  
+var pool = new Pool(config);
+app.get('/test-db', function(req, res) {
+    pool.query("SELECT * FROM test", function(err, result){
+        if(err){
+            res.status(500).send(err.toString());
+        }
+        else{
+            res.send(JSON.srtingifly(result));
+        }
+    });
+});
+  
 });
 app.get('/:articleName', function (req, res){
 //artileName == article-one
